acfe2d952b33c033c5f53cbb194def69d19b137e
rahafalwan <rahaf.alwan.prog@gmail.com>
2023-10-28 13:51:48 +0200

    second

--- javascript/script.js
+++ javascript/script.js
@@ -34,3 +34,30 @@ $(document).ready(function(){
        }
    });
  });
+ function SearchFunction(x) {
+    x.style.background = "white";
+  }
+  function restSearch(x) {
+    x.style.background = "transperent";
+  }
+  function validateForm() {
+   
+    if (isEmpty(document.getElementById('data_mail').value.trim())) {
+        alert('EMAIL is required!');
+        return false;
+        }
+        if (!validateEmail(document.getElementById('data_mail').value.trim())) {
+        alert('EMAIL must be a valid email address!');
+        return false;
+        }
+        return true;
+        }
+
+        function isEmpty(str)
+         {
+             return (str.length === 0 || !str.trim()); 
+        }
+        function validateEmail(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,15}(?:\.[a-z]{2})?)$/i;
+        return isEmpty(email) || re.test(email);
+        }
